7b472f44f1bf656c92df8438fd7765233ccec63e
= <czerwiecr1999@gmail.com>
2022-12-13 04:34:54 +0100

    Added bouncing ending panel

--- src/components/EndingPanel.js
+++ src/components/EndingPanel.js
@@ -7,7 +7,7 @@ function EndingPanel({text, red, green}){
     const classes = ClassNames({'text-red-500': red, 'text-lime-400':green})
     
     return(    
-    <div className="flex border p-2 flex-col items-center text-2xl m-auto">
+    <div className="flex border p-2 flex-col items-center text-2xl m-auto animate-bounce">
     <div className={classes}>{text}</div>
     <div>
       {score.player2Score}-{score.player1Score}
